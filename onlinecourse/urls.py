7d8831c4bcd423af3cd21639703e4d060179ec66
PV <pv@Pavans-MacBook-Air.local>
2026-03-22 01:47:05 -0500

    Final fix with required repo name and Submission model

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -3,11 +3,8 @@ from . import views
 
 app_name = 'onlinecourse'
 urlpatterns = [
-    # This matches the page in your screenshot
     path('course/<int:course_id>/', views.submit, name='course_details'),
-    # This is where the "Take Exam" button goes
     path('course/<int:course_id>/submit/', views.submit, name='submit'),
-    # This is the result page
     path('course/<int:course_id>/submission/<int:submission_id>/result/', 
          views.show_exam_result, name='show_exam_result'),
 ]
