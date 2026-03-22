8378daf4d82bbc2f0afbaf7a06b74d061c7761f1
PV <pv@Pavans-MacBook-Air.local>
2026-03-22 01:20:55 -0500

    Fixed admin URL routing

--- final_project/urls.py
+++ final_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.register),
+    # Change admin.site.register to admin.site.urls
+    path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
 ]
