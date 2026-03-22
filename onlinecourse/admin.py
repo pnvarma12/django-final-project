c3ff124253b58a8e6ee23e7646ac2e2d04477d6e
PV <pv@Pavans-MacBook-Air.local>
2026-03-22 01:05:13 -0500

    Update admin.py for task 2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Enrollment, Submission
 
-# Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Course)
+admin.site.register(Submission)
